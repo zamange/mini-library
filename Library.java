178bce1ad9c9e52747219b9e4cc2c5f7eb8b8e24
WTC <zngeyane023@student.wethinkcode.co.za>
2024-05-02 12:23:02 +0200

    constructor

--- Library.java
+++ Library.java
@@ -1,5 +1,26 @@
 import java.util.ArrayList;
 
 public class Library {
-    ArrayList<Book> books = new ArrayList<Book>();
+    private ArrayList<Book> books;
+
+
+    //constructor
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook (Book book){
+        books.add(book);
+    }
+
+    public void removeBook (Book book){
+        books.remove(book);
+    }
+
+    public String searchByTitle(String title){
+        if (books.contains(title)){
+            return ;
+        }
+    }
 }
